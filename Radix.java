922110606355a865067554fb36bc36bb59ab481e
qiqimonkey4300 <yueqi04@gmail.com>
2021-01-13 19:26:11 -0500

    radixSort included

--- Radix.java
+++ Radix.java
@@ -36,7 +36,26 @@ public class Radix {
   }
 
   public static void radixSort(SortableLinkedList data) {
-
+    SortableLinkedList[] positiveBuckets = new SortableLinkedList[10];
+    SortableLinkedList[] negativeBuckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      positiveBuckets[i] = new SortableLinkedList();
+      negativeBuckets[i] = new SortableLinkedList();
+    }
+    int lsd = 0;
+    for (int i = 0; i < data.size(); i++) {
+      if (length(data.get(i)) > lsd) lsd = length(data.get(i));
+    }
+    for (int i = 0; i < lsd; i++) {
+      while (data.size() != 0) {
+        int digit = nth(data.get(0), i);
+        if (data.get(0) < 0) negativeBuckets[9 - Math.abs(digit)].add(data.get(0));
+        else positiveBuckets[digit].add(data.get(0));
+        data.remove(0);
+      }
+      merge(data, negativeBuckets);
+      merge(data, positiveBuckets);
+    }
   }
 
 }
